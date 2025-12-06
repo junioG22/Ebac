b0c28b9badc46a229029b2693a6b66dc52ae463f
junioG22 <junioggcosta@yahoo.com.br>
2025-12-06 18:24:09 -0300

    exercicio 3 ebac

--- src/Exercicio3.java
+++ src/Exercicio3.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Exercicio3 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite um número positivo: ");
+        int n = scanner.nextInt();
+
+        // CRESCENTE
+        System.out.println("\nOrdem crescente:");
+        for (int i = 0; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+
+        // DECRESCENTE
+        System.out.println("\n\nOrdem decrescente:");
+        for (int i = n; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+
+        // EXIBIR NOME
+        System.out.println("\n");
+
+        if (nome.length() > 6) {
+            for (int i = 0; i < n; i++) {
+                System.out.println(nome);
+            }
+        } else {
+            System.out.println(nome);
+        }
+
+        scanner.close();
+    }
 }
